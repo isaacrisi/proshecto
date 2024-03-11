88b4e2d7285fe26c1c4e8885c8b523d67bc187c8
sebastr008 <sebastiantorres506@gmail.com>
2024-03-11 07:34:16 -0500

    cambios clase 11/03/24

---
+++ Assets/Juego/Scripts/UI/botonInstrucciones.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botonInstrucciones : MonoBehaviour
+{
+   public void Onclicbotoninstrucciones()
+    {
+        Debug.Log("prueba");
+    }
+}
