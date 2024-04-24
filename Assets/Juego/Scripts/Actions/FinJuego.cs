f5c70a105672056a8910296ec0489775c1794dcc
EffyXIII <145070591+EffyXIII@users.noreply.github.com>
2024-04-24 07:45:41 -0500

    perder

--- Assets/Juego/Scripts/Actions/FinJuego.cs
+++ Assets/Juego/Scripts/Actions/FinJuego.cs
@@ -1,9 +1,13 @@
+using JetBrains.Annotations;
+using MoreMountains.Tools;
+using MoreMountains.TopDownEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FinJuego : MonoBehaviour
 {
+    //GameManager gameManager = new GameManager();
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,9 @@ public class FinJuego : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameManager.Instance.CurrentLives <=0)
+        {
+            MMSceneLoadingManager.LoadScene("GameOver");
+        }
     }
 }
